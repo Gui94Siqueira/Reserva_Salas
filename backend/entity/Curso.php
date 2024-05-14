9a039d294a6fdf438da144d7b378dad6773d000f
Guilherme <guilherme94siqueira@gmail.com>
2024-05-14 20:49:38 -0300

    entity Curso e sala

---
+++ backend/entity/Curso.php
@@ -0,0 +1,38 @@
+<?php
+
+    Class Curso {
+        private $id;
+        private $nome;
+        private $sigla;
+        private $subArea_Id;
+
+        public function __construct($id, $nome, $sigla, $subArea_Id) {
+            $this->id = $id;
+            $this->nome = $nome;
+            $this->sigla = $sigla;
+            $this->subArea_Id = $subArea_Id;
+        }
+
+        // GETTERS
+
+        public function getId() {
+            return $this->id;
+        }
+
+        public function getNome() {
+            return $this->nome;
+        }
+
+        public function getSigla() {
+            return $this->sigla;
+        }
+
+        public function getSubarea_Id() {
+            return $this->subArea_Id;
+        }
+
+
+        // SETTERS
+    }
+
+?>
--- backend/entity/Sala.php
+++ backend/entity/Sala.php
@@ -5,8 +5,31 @@
         private $tipo;
         private $capacidade;
         
-        public function __construct() {
-            
+        public function __construct($id, $numero, $tipo, $capacidade) {
+            $this->id = $id;
+            $this->numero = $numero;
+            $this->tipo = $tipo;
+            $this->capacidade = $capacidade;
         }
+
+        // GETTERS
+
+        public function getId() {
+            return $this->id;
+        }
+
+        public function getNumero() {
+            return $this->numero;
+        }
+
+        public function getTipo() {
+            return $this->tipo;
+        }
+
+        public function getCapacidade() {
+            return $this->capacidade;
+        }
+
+        // SETTERS
     }
 ?>
