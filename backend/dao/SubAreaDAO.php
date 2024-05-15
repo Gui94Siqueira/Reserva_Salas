1bcface929fc86570a337d9b804dd7a93582d023
Guilherme <guilherme94siqueira@gmail.com>
2024-05-15 19:57:01 -0300

    getall e getbyid subarea

--- backend/dao/SubAreaDAO.php
+++ backend/dao/SubAreaDAO.php
@@ -11,15 +11,45 @@
         }
 
         public function getById($id) {
+            try {
+                $sql = "SELECT * FROM subarea WHERE Id = :id";
+                $stmt = $this->db->prepare($sql);
+                $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+                $stmt->execute();
 
+                $turma = $stmt->fetch(PDO::FETCH_ASSOC);
+
+                return $turma ?
+                    new Turma($turma['Id'],
+                                $turma['Nome'],
+                                $turma['Cor'])
+                    : null;
+
+            } catch (PDOException $e) {
+                return false;
+            }
         }
 
         public function getAll() {
+            try {
+                $sql = "SELECT * FROM subarea";
+                $stmt = $this->db->prepare($sql);
+                $stmt->execute();
 
+                $subareas = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+                return array_map(function ($subarea) {
+                    return new Turma($subarea['Id'],
+                                     $subarea['Nome'],
+                                     $subarea['Cor']);
+                }, $subareas);
+            } catch (PDOException $e) {
+                return false;
+            }
         }
 
         public function create($entity) {
-
+            
         }
 
         public function update($entity) {
